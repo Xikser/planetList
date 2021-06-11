90e2fabb7a420b597e910d1588b71f969f8421b7
Xikser <51416960+Xikser@users.noreply.github.com>
2021-06-11 17:02:18 +0200

    Add h1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <section class="container">
+    <h1>Lista planet:</h1>
     <PlanetsList/>
   </section>
 </template>
@@ -14,3 +15,18 @@ export default {
   }
 }
 </script>
+
+<style lang="sass">
+.container
+  display: flex
+  flex-direction: column
+  justify-content: center
+  align-items: center
+
+  h1
+    font-size: 3.5rem
+    font-family: 'Helvetica',sans-serif
+    font-weight: 600
+    text-transform: uppercase
+    letter-spacing: 2.25px
+</style>
