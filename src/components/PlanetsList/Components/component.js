110ad32c1cfc67e5c42c0cfc8025b32dc3560628
Xikser <51416960+Xikser@users.noreply.github.com>
2021-06-11 17:02:43 +0200

    Create component file

---
+++ src/components/PlanetsList/Components/component.js
@@ -0,0 +1,28 @@
+// export default {
+//   props: {
+//     planetName: {
+//       type: String,
+//       required: true
+//     },
+//     planetRotation: {
+//       type: String,
+//       required: true
+//     },
+//     planetClimate: {
+//       type: String,
+//       required: true
+//     },
+//     planetGravity: {
+//       type: String,
+//       required: true
+//     },
+//     planetCreated: {
+//       type: String,
+//       required: true
+//     },
+//     planetUrl: {
+//       type: String,
+//       required: true
+//     }
+//   }
+// }
