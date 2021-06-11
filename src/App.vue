9aa9db72aad7fbc246650fbefc5add595b9c39c9
Xikser <51416960+Xikser@users.noreply.github.com>
2021-06-11 15:09:41 +0200

    Create basic css styles

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
-  <div id="app">
+  <main class="app">
     <router-view/>
-  </div>
+  </main>
 </template>
 
-<style lang="sass" scoped>
-#app
-  font-family: Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing: antialiased
-  -moz-osx-font-smoothing: grayscale
-  text-align: center
-  color: $main-color
+<style lang="sass">
+html
+  font-size: 10px
+  scroll-behavior: smooth
 
+*, *::before, *::after
+  box-sizing: border-box
+
+body
+  background: $third-color
+  overflow-x: hidden
+  width: 100vw
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  color: $text-color
+
+a
+  text-decoration: none
+  color: $text-color
+
+ol, ul, li
+  list-style: none
 </style>
