bdaa24cdcdaf54b1b0bb0b2c7bc081017c4c2d31
Xikser <51416960+Xikser@users.noreply.github.com>
2021-06-11 21:23:59 +0200

    Update files
    Include pagination component
    Cut pagination logic from PlanetList component to Home component

--- src/components/PlanetsList/component.js
+++ src/components/PlanetsList/component.js
@@ -1,32 +1,14 @@
 // @vue/component
 import PlanetItem from './Components/Planets/PlanetItem'
-import axios from 'axios'
-
-const URL = 'https://swapi.dev/api/planets'
 
 export default {
   name: 'PlanetsList',
   components: {
     PlanetItem
   },
-  data () {
-    return {
-      planets: []
-    }
-  },
-  mounted () {
-    this.getItems()
-  },
-  methods: {
-    getItems () {
-      axios.get(`${URL}`)
-        .then((response) => {
-          this.planets = response.data.results
-          console.log(this.planets)
-        })
-        .catch((error) => {
-          console.log(error)
-        })
+  props: {
+    planets: {
+      type: Array
     }
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,92 @@
 <template>
   <section class="container">
+
+    <Pagination
+      :current="pagination.current"
+      :pages="pagination.pages"
+      :prevButtonStatus="prevDisabled"
+      :nextButtonStatus="nextDisabled"
+      @currentPage="handleCurrentPage"
+      @paginationClick="handleCurrentPage"
+    />
+
     <h1>Lista planet:</h1>
-    <PlanetsList/>
+
+    <PlanetsList :planets="planets"/>
+
   </section>
 </template>
 
 <script>
 import PlanetsList from '@/components/PlanetsList'
+import axios from 'axios'
+
+const URL = 'https://swapi.dev/api/planets/?page='
 
 export default {
   name: 'Home',
   components: {
-    PlanetsList
+    PlanetsList,
+    Pagination: () => import('../components/PlanetsList/Components/Pagination/Pagination')
+  },
+  data () {
+    return {
+      pagination: {
+        prev: '',
+        next: '',
+        current: 1,
+        pages: 6
+      },
+      prevDisabled: false,
+      nextDisabled: false,
+      planets: []
+    }
+  },
+  mounted () {
+    this.getItems()
+  },
+  methods: {
+    async getItems () {
+      await axios.get(`${URL}${this.pagination.current}`)
+        .then((response) => {
+          this.prev = response.data.previous
+          this.next = response.data.next
+          this.planets = response.data.results
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+
+    handleCurrentPage (argument) {
+      if (argument === 'next') {
+        // next page
+        this.pagination.current++
+        this.getItems()
+      } else if (argument === 'prev') {
+        // previous page
+        this.pagination.current--
+        this.getItems()
+      } else {
+        // page id
+        this.pagination.current = argument
+        this.getItems()
+      }
+
+      if (this.pagination.current === 1) {
+        this.pagination.current = 1
+        this.prevDisabled = true
+      } else {
+        this.prevDisabled = false
+      }
+
+      if (this.pagination.current === 6) {
+        this.pagination.current = 6
+        this.nextDisabled = true
+      } else {
+        this.nextDisabled = false
+      }
+    }
   }
 }
 </script>
@@ -25,7 +100,7 @@ export default {
 
   h1
     font-size: 3.5rem
-    font-family: 'Helvetica',sans-serif
+    font-family: 'Helvetica', sans-serif
     font-weight: 600
     text-transform: uppercase
     letter-spacing: 2.25px
